34f588fc1c8e17d5298798d31861df2f8c2e5e75
Adan Hernandez <adanh74379@gmail.com>
2025-10-15 16:07:40 -0700

    Froggy and Palindrome exercises solved.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,35 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+const handleInput = function validateAndCheckPalindromeInputHandler(event) {
+  const inputNum = event.target.value;
+
+  if (inputNum < 0) {
+    document.getElementById("palindromeResult").textContent =
+      "Please enter a positive number.";
+    document.getElementById("palindromeResult").className = "text-warning";
+    return;
+  } else if (!/^\d*$/.test(inputNum) && inputNum.length > 0) {
+    document.getElementById("palindromeResult").textContent =
+      "Please enter a valid number.";
+    document.getElementById("palindromeResult").className = "text-warning";
+    return;
+  }
+
+  const reversedStr = inputNum.split("").reverse().join("");
+  const isPalindrome = inputNum === reversedStr;
+
+  const resultElem = document.getElementById("palindromeResult");
+
+  if (isPalindrome && inputNum.length > 0) {
+    resultElem.textContent = "Yes. This is a palindrome!";
+    resultElem.className = "text-success";
+  } else if (inputNum.length === 0) {
+    resultElem.textContent = "";
+    resultElem.className = "";
+  } else {
+    resultElem.textContent = "No. Try Again.";
+    resultElem.className = "text-danger";
+  }
+};
+
+elem.addEventListener("input", handleInput);
